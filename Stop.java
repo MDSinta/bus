9aff2d65d28af092749f5da53c9effe2b7b69146
MDSinta <106995098+MDSinta@users.noreply.github.com>
2024-01-17 09:51:33 -0500

    Update Stop.java

--- Stop.java
+++ Stop.java
@@ -1,7 +1,7 @@
 public class Stop {
     Bus bus;
     int travelTime = Math.random()*5-2+1;
-    Passenger[] passengers
+    Passenger[] passengers = new Passenger[];
     public void checkBus() {
         if 
     }
